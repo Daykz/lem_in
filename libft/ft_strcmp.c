e7e9e3fe85edbc41a351d806917168f84b787992
David MATHE <dmathe@e1r3p4.42.fr>
2016-03-15 08:58:22 +0100

    lemin

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: sksourou <sksourou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/26 23:13:58 by sksourou          #+#    #+#             */
-/*   Updated: 2015/01/26 23:13:59 by sksourou         ###   ########.fr       */
+/*   Updated: 2016/03/15 08:57:51 by dmathe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	int i;
+	int		counter;
 
-	i = 0;
-
-	while (s1[i] && s2[i] && s1[i] == s2[i])
-		i++;
-	if (s1[i] == '\0' && s2[i] == '\0')
+	if (s1 == NULL && s2 == NULL)
 		return (0);
-	return (1);
+	if (s1 == NULL)
+		return (-1);
+	if (s2 == NULL)
+		return (1);
+	counter = 0;
+	while (s1[counter] == s2[counter] && s1[counter] != '\0')
+		counter++;
+	return ((s1[counter] - s2[counter]));
 }
